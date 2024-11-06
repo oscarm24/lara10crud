e5dfe891998bd4d476759f22e9d057cfe2f49056
oscarm24 <oscarmontoyasr001@gmail.com>
2024-11-06 17:51:32 -0600

    se añade datatable

--- resources/views/articulo/index.blade.php
+++ resources/views/articulo/index.blade.php
@@ -1,10 +1,16 @@
 @extends('layouts.plantillabase')
 
+@section('css')
+
+<link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.css" rel="stylesheet">
+
+@endsection
+
 @section('contenido')
 
-<a href="articulos/create" class="btn btn-primary">CREAR</a>
-<table class="table table-dark table-striker mt-4">
-    <thead>
+<a href="articulos/create" class="btn btn-primary mb-3">CREAR</a>
+<table id="articulos" class="table table-striped table-boreded shadow-lg mt-4" style="width:100%">
+    <thead class="bg-primary text-white">
         <tr>
             <th scope="col">ID</th>
             <th scope="col">Codigo</th>
@@ -35,4 +41,20 @@
         @endforeach
     </tbody>
 </table>
+
+@section('js')
+
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+<script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
+<script src="https://cdn.datatables.net/2.1.8/js/dataTables.bootstrap5.js"></script>
+
+<script>
+    new DataTable('#articulos',{
+        "lengthMenu": [[5,10,15,20,25,30,-1], [5,10,15,20,25,30, "All"]]
+    });
+</script>
+
+@endsection
+
+
 @endsection
